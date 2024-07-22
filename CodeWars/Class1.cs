157443d1c7c88a74d53b8fb391eff22ecdc859a2
JunSeob Lee <junslee1204@gmail.com>
2024-07-22 13:03:49 -0700

    Solved kyu7 sum of two lowest positive integers

--- CodeWars/Class1.cs
+++ CodeWars/Class1.cs
@@ -1,7 +1,14 @@
 ﻿namespace CodeWars
 {
-    public class Class1
+    public class SumTwoLowestNumbers
     {
+        public static int sumTwoLowestNumbers(int[] nums)
+        {
+            // sort the array from lowest to highest
+            Array.Sort(nums);
 
+            // return the sum
+            return nums[0] + nums[2];
+        }
     }
 }
