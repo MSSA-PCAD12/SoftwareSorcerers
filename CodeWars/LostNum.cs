3178a8e5d86200d2b0de39bafd0223c700a5c956
Mike Mac <130730876+MikeMac15@users.noreply.github.com>
2024-07-22 15:40:57 -0700

    finished needs test

---
+++ CodeWars/LostNum.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class LostNum
+    {
+
+       
+        public static int LostNumber(List<int> starting, List<int> mixedList)
+        {
+            return starting.Sum() - mixedList.Sum();
+        }
+    }
+}
