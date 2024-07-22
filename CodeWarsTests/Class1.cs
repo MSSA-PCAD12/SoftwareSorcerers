b2f2683439576a481cac3333ecd2a2b5dd97eb04
Mike Mac <130730876+MikeMac15@users.noreply.github.com>
2024-07-22 11:47:44 -0700

    new

--- CodeWarsTests/Class1.cs
+++ CodeWarsTests/Class1.cs
@@ -2,6 +2,13 @@
 {
     public class Class1
     {
+        public static int SumLowestTwoNums(int[] nums)
+        {
+            int num1 = 0;
+            int num2 = 0;
+
+            return num1 + num2;
+        }
 
     }
 }
