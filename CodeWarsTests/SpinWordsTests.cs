33edde3cea1a0b49e0225f3a705832e30cc24633
montkat <175044201+montkat@users.noreply.github.com>
2024-07-25 13:48:53 -0500

    Test edit

--- CodeWarsTests/SpinWordsTests.cs
+++ CodeWarsTests/SpinWordsTests.cs
@@ -12,59 +12,46 @@ namespace CodeWars.Tests
 
     public class SpinWordsTests
         {
-
             [TestMethod()]
             public static void SpinWordsMainTest1()
 
             {
-
                 Assert.AreEqual("emocleW", SpinWords.SpinWordsMain("Welcome"));
-
             }
 
             [TestMethod()]
             public static void Test2()
 
             {
-
                 Assert.AreEqual("Hey wollef sroirraw", SpinWords.SpinWordsMain("Hey fellow warriors"));
-
             }
 
             [TestMethod()]
             public static void Test3()
 
             {
-
                 Assert.AreEqual("This is a test", SpinWords.SpinWordsMain("This is a test"));
-
             }
 
             [TestMethod()]
             public static void Test4()
 
             {
-
                 Assert.AreEqual("This is rehtona test", SpinWords.SpinWordsMain("This is another test"));
-
             }
 
             [TestMethod()]
             public static void Test5()
 
             {
-
                 Assert.AreEqual("You are tsomla to the last test", SpinWords.SpinWordsMain("You are almost to the last test"));
-
             }
 
             [TestMethod()]
             public static void Test6()
 
             {
-
                 Assert.AreEqual("Just gniddik ereht is llits one more", SpinWords.SpinWordsMain("Just kidding there is still one more"));
-
             }
 
         }
