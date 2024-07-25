6533c5020148110548f4547561c27d7586916667
montkat <175044201+montkat@users.noreply.github.com>
2024-07-25 13:36:43 -0500

    SpinWords

---
+++ CodeWars/SpinWords.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars
+{
+    public class SpinWords
+    {
+        public static string SpinWordsMain(string sentence)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> words = Extension.Split(sentence);
+
+            foreach (string word in words)
+            {
+                if (word.Length >= 5)
+                {
+                    string revWord = Extension.ReverseWord(word);
+                    sb.Append(revWord);
+                }
+                else
+                {
+                    sb.Append(word);
+                }
+                sb.Append(' ');
+            }
+            return Extension.Join(sb);
+        }
+    }
+        public static class Extension
+    {
+        public static List<string> Split(this string sentence)
+        {
+            StringBuilder sb = new StringBuilder();
+            List<string> strList = new List<string>();
+            foreach (char letter in sentence)
+            {
+                if (letter != ' ')
+                {
+                    sb.Append(letter);
+                }
+                else
+                {
+                    strList.Add(sb.ToString());
+                    sb.Clear();
+                }
+            }
+            strList.Add(sb.ToString());
+            return strList;
+        }
+        public static string Join(StringBuilder sb)
+        {
+            string str = "";
+            for (int i = 0; i < sb.Length; i++)
+            {
+                if (i != sb.Length - 1)
+                {
+                    str += sb[i];
+                }
+                if (i == sb.Length - 1 && sb[i] != ' ')
+                {
+                    str += sb[i];
+                }
+            }
+            return str;
+        }
+        //   public static string Join(StringBuilder sb) { // needs to be a char array
+        //     var phrase = sb.Aggregate((partialPhrase, word) => $"{partialPhrase} {word}");
+        //     return phrase;
+        //     }
+        public static string ReverseWord(string word)
+        {
+            StringBuilder sb = new StringBuilder();
+            char[] charArray = word.ToCharArray();
+            for (int i = charArray.Length - 1; i >= 0; i--)
+            {
+                sb.Append(charArray[i]);
+            }
+            //         return sb.ToString();
+            return Join(sb);
+        }
+    }
+}
+
---
+++ CodeWarsTests/SpinWordsTests.cs
@@ -0,0 +1,72 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using CodeWars;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeWars.Tests
+{
+    [TestClass()]
+
+    public class SpinWordsMainTests
+        {
+
+            [TestMethod()]
+            public static void Test1()
+
+            {
+
+                Assert.AreEqual("emocleW", SpinWords.SpinWordsMain("Welcome"));
+
+            }
+
+            [TestMethod()]
+            public static void Test2()
+
+            {
+
+                Assert.AreEqual("Hey wollef sroirraw", SpinWords.SpinWordsMain("Hey fellow warriors"));
+
+            }
+
+            [TestMethod()]
+            public static void Test3()
+
+            {
+
+                Assert.AreEqual("This is a test", SpinWords.SpinWordsMain("This is a test"));
+
+            }
+
+            [TestMethod()]
+            public static void Test4()
+
+            {
+
+                Assert.AreEqual("This is rehtona test", SpinWords.SpinWordsMain("This is another test"));
+
+            }
+
+            [TestMethod()]
+            public static void Test5()
+
+            {
+
+                Assert.AreEqual("You are tsomla to the last test", SpinWords.SpinWordsMain("You are almost to the last test"));
+
+            }
+
+            [TestMethod()]
+            public static void Test6()
+
+            {
+
+                Assert.AreEqual("Just gniddik ereht is llits one more", SpinWords.SpinWordsMain("Just kidding there is still one more"));
+
+            }
+
+        }
+
+    }
