1f4108fa41527445d404a663e33647548d697f66
montkat <175044201+montkat@users.noreply.github.com>
2024-07-28 16:22:05 -0500

    Created RPN Calculator and Test

---
+++ CodeWars/RPN.cs
@@ -0,0 +1,54 @@
+﻿using System.Linq.Expressions;
+
+namespace RPN
+{
+    public class RPNCalc
+    {
+        static void Main()
+        {
+            //create a calculator that evaluates expressions in RPN
+            // 5*2 5 2 *
+            //push 5 onto stack, push 2 onto stack, encounter *, pop 5 and 2, compute 5 * 2 = 10, push 10 onto stack
+        }
+        public static double Evaluation(string Expression)
+        {
+            var stack = new Stack<double>();
+
+            foreach (var token in Expression.Split(' '))
+            {
+                double result;
+                if (double.TryParse(token, out result))
+                {
+                    stack.Push(result);
+                }
+                else
+                {
+                    if (stack.Count == 0)
+                    {
+                        return 0;
+                    }
+                    double rightOperant = stack.Pop();
+                    double leftOperant = stack.Pop();
+                    switch (token)
+                    {
+                        case "+":
+                            stack.Push(leftOperant + rightOperant);
+                            break;
+                        case "-":
+                            stack.Push(leftOperant - rightOperant);
+                            break;
+                        case "*":
+                            stack.Push(leftOperant * rightOperant);
+                            break;
+                        case "/":
+                            stack.Push(leftOperant / rightOperant);
+                            break;
+                        default:
+                            throw new Exception("Invalid operator");
+                    }
+                }
+            }
+            return stack.Pop();
+        }
+    }
+}
---
+++ CodeWarsTests/RPNCalcTests.cs
@@ -0,0 +1,66 @@
+﻿using RPN;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RPN.Tests
+{
+    [TestClass()]
+    public class ProgramTests
+    {
+        [TestMethod()]
+        public void EvaluationTest()
+        {
+            //Arrange
+            string rpnExpression = " ";
+            double expectedResult = 0;
+            //Act
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            //Assert
+            Assert.AreEqual(expectedResult, actualResult);
+
+        }
+        [TestMethod()]
+        public void EvaluationTest2()
+        {
+            string rpnExpression = "3";
+            double expectedResult = 3;
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod()]
+        public void EvaluationTest3()
+        {
+            string rpnExpression = "1 3 +";
+            double expectedResult = 4;
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod()]
+        public void EvaluationTest4()
+        {
+            string rpnExpression = "1 3 *";
+            double expectedResult = 3;
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod()]
+        public void EvaluationTest5()
+        {
+            string rpnExpression = "1 3 -";
+            double expectedResult = -2;
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+        [TestMethod()]
+        public void EvaluationTest6()
+        {
+            string rpnExpression = "4 2 /";
+            double expectedResult = 2;
+            double actualResult = RPNCalc.Evaluation(rpnExpression);
+            Assert.AreEqual(expectedResult, actualResult);
+        }
+    }
+}
