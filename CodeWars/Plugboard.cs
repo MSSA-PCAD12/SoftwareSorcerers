009bd800f16ed4357ddc60b4a96e3abad353a8e5
Caleb Joseph <175043934+Caleb-A-J@users.noreply.github.com>
2024-07-31 14:46:38 -0700

    Took a shot at Plugboard...

---
+++ CodeWars/Plugboard.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class Plugboard
+    {
+        private readonly Dictionary<char, char> _wiring;
+        public Plugboard(string wiringPairs)
+        {
+            if (string.IsNullOrEmpty(wiringPairs))
+                throw new ArgumentNullException(nameof(wiringPairs), "Wiring pairs cannot be null or empty.");
+            if (wiringPairs.Length % 2 != 0)
+                throw new ArgumentException("Wiring pairs must be in even numbers.", nameof(wiringPairs));
+
+            _wiring = GenerateWiring(wiringPairs);
+        }
+        private Dictionary<char, char> GenerateWiring(string wiringPairs)
+        {
+            var wiring = new Dictionary<char, char>();
+            for (int i = 0; i < wiringPairs.Length; i += 2)
+            {
+                char a = wiringPairs[i];
+                char b = wiringPairs[i + 1];
+
+                ValidateCharacters(a, b);
+                if (wiring.ContainsKey(a) || wiring.ContainsKey(b))
+                    throw new ArgumentException("Duplicate or conflicting wiring detected.");
+
+                wiring[a] = b;
+                wiring[b] = a;
+            }
+            return wiring;
+        }
+        private void ValidateCharacters(char a, char b)
+        {
+            if (!IsValidCharacter(a) || !IsValidCharacter(b))
+                throw new ArgumentException("Wiring pairs must only contain letters A-Z.");
+        }
+        public char Process(char input)
+        {
+            return _wiring.TryGetValue(input, out char output) ? output : input;
+        }
+
+        private bool IsValidCharacter(char c)
+        {
+            return c >= 'A' && c <= 'Z';
+        }
+    }
+}
---
+++ CodeWarsTests/PlugboardTest.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class PlugboardTests
+    {
+        [TestMethod]
+        public void Test_ValidWiringPairs()
+        {
+            var plugboard = new Plugboard("ABCD");
+            Assert.AreEqual('B', plugboard.Process('A')); 
+            Assert.AreEqual('A', plugboard.Process('B')); 
+            Assert.AreEqual('D', plugboard.Process('C')); 
+            Assert.AreEqual('C', plugboard.Process('D')); 
+            Assert.AreEqual('X', plugboard.Process('X')); 
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_InvalidWiringPairs_ThrowsException()
+        {
+
+            var plugboard = new Plugboard("ABC");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Test_InvalidCharacterInWiringPairs_ThrowsException()
+        {
+
+            var plugboard = new Plugboard("AB1C");
+        }
+
+        [TestMethod]
+        public void Test_ProcessingNonAlphabetCharacter()
+        {
+            var plugboard = new Plugboard("ABCD");
+            Assert.AreEqual('.', plugboard.Process('.')); 
+        }
+    }
+}
