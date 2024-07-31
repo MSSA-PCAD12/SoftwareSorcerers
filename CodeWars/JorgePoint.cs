d3047110e56d7529daa76bf9279b1e677fd37b5d
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-07-31 13:27:37 -0500

    point test jorge

---
+++ CodeWars/JorgePoint.cs
@@ -0,0 +1,37 @@
+﻿
+namespace Pointsin3DSpace
+{
+    public class Point
+    {
+        public double X { get; set; }
+        public double Y { get; set; }
+        public double Z { get; set; }
+
+
+        public Point(double x, double y, double z)
+        {
+            this.X = x;
+            this.Y = y;
+            this.Z = z;
+        }
+
+        public static Point GetOrigin()
+        {
+            return new Point(0, 0, 0);
+        }
+        public double GetDistanceFromOrigin()
+        {
+
+            return Math.Sqrt(X * X + Y * Y + Z * Z);
+        }
+        public double GetDistanceFromPoint(Point other)
+        {
+            double dx = X - other.X;
+            double dy = Y - other.Y;
+            double dz = Z - other.Z;
+            return Math.Sqrt(dx * dx + dy * dy + dz * dz);
+        }
+
+
+    }
+}
