8d5fc89bcb9ce97cd9f85aab4f5a1e4b89d8f527
Jorge Quintero <175043690+JorgeQ2@users.noreply.github.com>
2024-07-31 16:46:35 -0500

    plugboard quintero

---
+++ CodeWars/PlugBoard.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace CodeWars
+{
+    public class Plugboard
+    {
+        private Dictionary<char, char>? wiring;
+
+        public Plugboard(string wirePairs = "")
+        {
+            wiring = new Dictionary<char, char>();
+
+            if (!string.IsNullOrEmpty(wirePairs))
+            {
+                if (wirePairs.Length % 2 != 0)
+                {
+                    throw new ArgumentException("Wire pairs string must have an even number of characters");
+                }
+                if (wirePairs.Length > 20)
+                {
+                    throw new ArgumentException("Too many");
+                }
+
+                for (int i = 0; i < wirePairs.Length; i += 2)
+                {
+                    char first = wirePairs[i];
+                    char second = wirePairs[i + 1];
+
+                    // Check if the pair is the same
+                    if (first == second)
+                    {
+                        throw new ArgumentException("Cannot pair a character with itself");
+                    }
+
+                    // Check if either character is already paired
+                    if (wiring.ContainsKey(first) || wiring.ContainsKey(second))
+                    {
+                        throw new ArgumentException("Already paired");
+                    }
+
+                    wiring[first] = second;
+                    wiring[second] = first;
+                }
+            }
+        }
+
+        public char Process(char input)
+        {
+            if (wiring.ContainsKey(input))
+            {
+                return wiring[input];
+            }
+            return input;
+        }
+    }
+}
---
+++ CodeWarsTests/PlugBoardTest1.cs
@@ -0,0 +1,71 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using CodeWars;
+
+namespace CodeWarsTests
+{
+    [TestClass]
+    public class PlugboardTests
+    {
+        [TestMethod]
+        public void Constructor_EmptyString_ShouldInitializeEmptyWiring()
+        {
+            // Arrange
+            var plugboard = new Plugboard("");
+
+            // Act & Assert
+            Assert.IsNotNull(plugboard);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_OddLengthString_ShouldThrowArgumentException()
+        {
+            // Arrange & Act
+            var plugboard = new Plugboard("ABC");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_TooManyPairs_ShouldThrowArgumentException()
+        {
+            // Arrange & Act
+            var plugboard = new Plugboard("ABCDEFGHIJKLMNOPQRSTU");
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException))]
+        public void Constructor_DuplicatePairs_ShouldThrowArgumentException()
+        {
+            // Arrange & Act
+            var plugboard = new Plugboard("AABB");
+        }
+
+        [TestMethod]
+        public void Process_InputMappedCharacter_ShouldReturnMappedCharacter()
+        {
+            // Arrange
+            var plugboard = new Plugboard("AB");
+
+            // Act
+            var result = plugboard.Process('A');
+
+            // Assert
+            Assert.AreEqual('B', result);
+        }
+
+        [TestMethod]
+        public void Process_InputUnmappedCharacter_ShouldReturnSameCharacter()
+        {
+            // Arrange
+            var plugboard = new Plugboard("AB");
+
+            // Act
+            var result = plugboard.Process('C');
+
+            // Assert
+            Assert.AreEqual('C', result);
+        }
+    }
+}
